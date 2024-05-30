568901aec75d34e033d69681db672f144285e904
Mika Dekker <Mika@Pauls-iMac.local>
2024-05-30 20:38:09 +0200

    Added scss, nav, routing and pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root to: 'pages#home'
+  get 'pages/about'
+  get 'pages/projects'
+  get 'pages/contact'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
