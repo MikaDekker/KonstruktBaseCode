250d373f96f0c00c12bbf62a8e344f51d02a2e56
Mika Dekker <Mika@Pauls-iMac.local>
2024-05-31 22:07:01 +0200

    Added individual project pages, fixed responsiveness

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,18 @@ Rails.application.routes.draw do
   get '/projects', to: 'pages#projects'
   get '/about', to: 'pages#about'
   get '/contact', to: 'pages#contact'
-  get '/page1', to: 'pages#projectone'
+  get '/p1', to: 'pages#project1'
+  get '/p2', to: 'pages#project2'
+  get '/p3', to: 'pages#project3'
+  get '/p4', to: 'pages#project4'
+  get '/p5', to: 'pages#project5'
+  get '/p6', to: 'pages#project6'
+  get '/p7', to: 'pages#project7'
+  get '/p8', to: 'pages#project8'
+  get '/p9', to: 'pages#project9'
+
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
